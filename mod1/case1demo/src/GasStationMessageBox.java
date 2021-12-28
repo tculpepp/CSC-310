b25884ee486f4c505cdcc1c121b37ab04fceb195
Tom Culpepper <tculpepp@gmail.com>
2021-12-28 09:33:05 -0600

    case1demo code fixed and working

--- mod1/case1demo/src/GasStationMessageBox.java
+++ mod1/case1demo/src/GasStationMessageBox.java
@@ -1,28 +1,25 @@
 import javax.swing.JOptionPane; // Needed for the Scanner class
 
-    public class GasStationMessageBox
-    {
-        public static void main(String[] args)
-        {
-            String custInput;// To hold the user's input
-            char gasGrade; // choice of gas
-            double unleadPrice = 2.6;
-            double regularPrice = 3.2;
-            double premPrice = 3.8;
-            double total = 0.0;
-            boolean moreOrder = true;
-            //Prompt the price for the gas
-            JOptionPane.showMessageDialog(null, "Three kinds of gas available: \n" + "Unlead: $"+ unleadPrice);
+public class GasStationMessageBox {
+    public static void main(String[] args) {
+        String custInput;// To hold the user's input
+        char gasGrade; // choice of gas
+        double unleadedPrice = 2.6;
+        double regularPrice = 3.2;
+        double premPrice = 3.8;
+        double total = 0.0;
+        boolean moreOrder = true;
+        //Prompt the price for the gas
+        JOptionPane.showMessageDialog(null, "Three kinds of gas available: \n" + "Unleaded: $"+ unleadedPrice +"\nRegular: $"+ regularPrice +"\nPremium: $"+ premPrice);
 
-        while (moreOrder)
-        {
-            custInput = JOptionPane.showInputDialog("Press U for unlead, R for regular, and P for premium");
+        while (moreOrder) {
+            custInput = JOptionPane.showInputDialog("Press U for unleaded, R for regular, and P for premium");
             gasGrade = custInput.charAt(0);
             
             if (gasGrade == 'U'|| gasGrade == 'u')
             {
                 custInput = JOptionPane.showInputDialog("How many gallons of gas you want to purchase? ");
-                total = unleadPrice * Double.parseDouble(custInput);
+                total = unleadedPrice * Double.parseDouble(custInput);
                 JOptionPane.showMessageDialog(null,"Total is $" + total + ". Thank you for your business");
             }
             else if (gasGrade == 'R'|| gasGrade =='r')
@@ -43,6 +40,6 @@ import javax.swing.JOptionPane; // Needed for the Scanner class
             }
             else
                 JOptionPane.showMessageDialog(null, "Invalid choice, try again. ");
-            }
-}
+        }
+    }
 }
