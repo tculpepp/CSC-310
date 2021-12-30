889ac30ed58dcb179820e1a7619265f983887dc7
Tom Culpepper <tculpepp@gmail.com>
2021-12-30 10:55:42 -0600

    v1.0

--- mod1/case1/src/CalcTaxes.java
+++ mod1/case1/src/CalcTaxes.java
@@ -1,47 +1,64 @@
-// Write a Java application to calculate how much federal and state tax you need to pay. 
-// The program should accomplish the following tasks: ask your name, yearly income, federal 
-// tax rate, and state tax rate, then calculate and display the amount of tax you need to pay. 
-// Your program must catch user input errors. As the examples given above, use a dialog window 
-// to enter program input instead of console-based commands.
+/* 
+* Write a Java application to calculate how much federal and state tax you need to pay. 
+* The program should accomplish the following tasks: ask your name, yearly income, federal 
+* tax rate, and state tax rate, then calculate and display the amount of tax you need to pay. 
+* Your program must catch user input errors. As the examples given above, use a dialog window 
+* to enter program input instead of console-based commands.
+ */
 import javax.swing.JOptionPane;
+import java.util.regex.*;
 
 public class CalcTaxes {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         boolean calcAgain = true; //control to run again or exit
-        double yearlyIncome;
-        double fedTaxRate;
-        double stateTaxRate;
-        String username;
-        String userInput; //temporary holding for user input
+        String validationPattern;
+        String errorMessage;
+        String[] userInputs = new String[4];
+        String[][] inputRequests = {
+            {"Please enter your name:","str"},
+            {"Enter your yearly income","num"},
+            {"Enter your Federal tax rate (%):","num"},
+            {"Enter your State tax rate (%):","num"}
+        };
 
         JOptionPane.showMessageDialog(null, "This program will calculate your federal and state taxes");
 
         while (calcAgain) {
-            userInput = JOptionPane.showInputDialog("Please enter your name:");
-            username = userInput;
-            // add validation and exception check
+            for (int i=0; i < inputRequests.length; i++) {
+                userInputs[i] = JOptionPane.showInputDialog(inputRequests[i][0], JOptionPane.QUESTION_MESSAGE);
+                if (inputRequests[i][1].equals("num")){
+                    validationPattern = "[A-Za-z&%$#@!()*^, ]"; // allows only numbers plus , .
+                    errorMessage = "Please enter a number";
+                }
+                else if (inputRequests[i][1].equals("str")) {
+                    validationPattern = "[0-9&%$#@!()*^]"; // allows only letters and spaces
+                    errorMessage = "No numbers or special characters please";
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Illegal Input type Option. Please contact the developer.");
+                    throw new IllegalArgumentException("Input type option invalid. Only 'num' or 'str' allowed");
+                }
+                Pattern p = Pattern.compile(validationPattern);
+                Matcher m = p.matcher(userInputs[i]);
+                if (m.find() || userInputs[i].isBlank()){ 
+                    JOptionPane.showMessageDialog(null, errorMessage);
+                    i--;
+                    continue;
+                }
+            }
 
-            userInput = JOptionPane.showInputDialog("Enter your yearly income?");
-            yearlyIncome = Double.parseDouble(userInput);
-            // add validation and exception check
-
-            userInput = JOptionPane.showInputDialog("Enter your Federal tax rate (%):");
-            fedTaxRate = Double.parseDouble(userInput)/100;
-            // add validation and exception check
-
-            userInput = JOptionPane.showInputDialog("Enter your State tax rate (%):");
-            stateTaxRate = Double.parseDouble(userInput)/100;
-            // add validation and exception check
-            
+            double yearlyIncome = Double.parseDouble(userInputs[1]);
+            double fedTaxRate = Double.parseDouble(userInputs[2]) / 100;
+            double stateTaxRate = Double.parseDouble(userInputs[3]) / 100;
             double fedTaxDue = yearlyIncome * fedTaxRate;
             double stateTaxDue = yearlyIncome * stateTaxRate;
 
-            JOptionPane.showMessageDialog(null, username + "\nYour Federal Taxes are: $" + fedTaxDue + "\nYour State taxes are: $" + stateTaxDue);
+            JOptionPane.showMessageDialog(null, userInputs[0] + "\nYour Federal Taxes are: $" + fedTaxDue + "\nYour State taxes are: $" + stateTaxDue);
 
-            int reply = JOptionPane.showConfirmDialog(null, "Would you like to calculate more?", "title",  JOptionPane.YES_NO_OPTION);
+            int reply = JOptionPane.showConfirmDialog(null, "Would you like to calculate more?", "Run Again?",  JOptionPane.YES_NO_OPTION);
             if (reply != JOptionPane.YES_OPTION) {
-                 calcAgain = false;
+                    calcAgain = false;
             }
         }
-    }
+    } 
 }
