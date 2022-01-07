2ee426fbbd88bb65c2a007634fc22c83f59c09fd
Tom Culpepper <tculpepp@gmail.com>
2022-01-06 20:10:16 -0600

    added framework for mod2 assignments

---
+++ mod2/case2/src/ArraySearch.java
@@ -0,0 +1,17 @@
+/* 
+Write a Java application that performs the following task:
+
+1. Create an int array (you can declare the values yourself, or use the Random feature in java to create this array)
+2. Sort the data.
+3. Prompt user to input an integer from the keyboard
+4. Search for the user input value in the array created in step 1 using a simple linear search.
+5. Display a message whether the user input has been found in the array, at what position, and how many steps the program required to search for the data value.
+6. Search for the user input value in the array created in step 1 using another (hopefully more efficient) search algorithm.
+7. Display a message whether the user input has been found in the array, at what position, and how many steps the program required to search for the data value. 
+*/
+
+public class ArraySearch {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ mod2/slp2/src/HomeValue.java
@@ -0,0 +1,14 @@
+/* 
+Write a program for a real estate agent. The program should perform the following tasks:
+
+1. Create an array to hold average house price for the each of past 25 years for a single family residence of 1500 square feet. Initialize the array with the values in sorted order, assuming the average house price increases each year.  
+   Prompt the user for a house value.
+2. Use a binary search algorithm to determine the year that most closely matches the house value entered by the user and display the year and average house value for that year.  
+ */
+
+
+public class HomeValue {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
