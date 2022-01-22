425a96ce590733f54731f75128b61976d4eb0ff0
Tom Culpepper <tculpepp@gmail.com>
2022-01-21 20:10:13 -0600

    corrected encoding error in .csv. initial working HomeValue.java

--- mod2/slp2/src/HomeValue.java
+++ mod2/slp2/src/HomeValue.java
@@ -11,21 +11,50 @@ import java.io.IOException;
 
 public class HomeValue {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[][] homeValues = csvToArray("AverageHomeValues2021-1997.csv");
+        int target = 200000;
+        int bestYear = binarySearchClosest(homeValues, target);
+        System.out.println(bestYear);
 
-        try (BufferedReader br = new BufferedReader(new FileReader("AverageHomeValues2021-1997.csv"))) {
-
+    }
+    private static int[][] csvToArray(String filename) throws IOException {
+        int[][] outArray = new int[25][2];
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             String strCurrentLine;
-            String[][] homeValues = new String[25][];
-         
+            int i = 0;
             while ((strCurrentLine = br.readLine()) != null) {
-                homeValues[0] = strCurrentLine.split(",");
+                outArray[i][0] = Integer.parseInt(strCurrentLine.split(",")[0]);
+                outArray[i][1] = Integer.parseInt(strCurrentLine.split(",")[1]);
+                i++;
             }
-            for (String i : homeValues[0]){
-                System.out.println(i);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return outArray;
+    }
+
+    public static int binarySearchClosest(int[][] array, int target) {
+        if(target < array[0][1]) {
+            return array[0][0];
+        }
+        if(target > array[array.length-1][1]) {
+            return array[array.length-1][0];
+        }
+        int low = 0;
+        int high = array.length - 1;
+        int mid=0;
+        while (high - low != 1) {
+            mid = (high + low) / 2;
+            if (array[mid][1] == target) {
+                return array[mid][0];
             }
-            } catch (IOException e) {
-                e.printStackTrace();
+            else if (array[mid][1] > target) { //eliminate the right sub-array and continue with the left sub-array
+                high = mid;
             }
+            else { //mid < target, eliminate the left sub-array and continue with the right sub-array
+                low = mid;
+            }
+        }
+        return Math.abs(array[low][1] - target) <= Math.abs(target - array[high][1]) ? array[low][0] : array[high][0];
     }
 }    
