22fcb0497b0d3603f0dbf75542e51decf07ce4f9
Tom Culpepper <tculpepp@gmail.com>
2022-01-22 21:12:35 -0600

    framework for mod3

---
+++ mod3/slp3/src/ArrayLists.java
@@ -0,0 +1,9 @@
+/*
+ * Write a Java program that inserts 25 random integers ranging from 0 to 100 into an ArrayList in
+ * ascending order. The program should then output the numbers and display the sum and average of
+ * the numbers.
+ */
+
+public class ArrayLists {
+
+}
