4584c7b490d3290d249329f4d76b3c904d43c7cb
Tom Culpepper <tculpepp@gmail.com>
2022-01-22 20:03:39 -0600

    v1.0 submitted

--- mod2/slp2/src/HomeValue.java
+++ mod2/slp2/src/HomeValue.java
@@ -1,9 +1,12 @@
-/* 
-Write a program for a real estate agent. The program should perform the following tasks:
 
-1. Create an array to hold average house price for the each of past 25 years for a single family residence of 1500 square feet. Initialize the array with the values in sorted order, assuming the average house price increases each year.  
-   Prompt the user for a house value.
-2. Use a binary search algorithm to determine the year that most closely matches the house value entered by the user and display the year and average house value for that year.  
+/*
+ * Write a program for a real estate agent. The program should perform the following tasks:
+ * 
+ * 1. Create an array to hold average house price for the each of past 25 years for a single family
+ * residence of 1500 square feet. Initialize the array with the values in sorted order, assuming the
+ * average house price increases each year. Prompt the user for a house value. 2. Use a binary
+ * search algorithm to determine the year that most closely matches the house value entered by the
+ * user and display the year and average house value for that year.
  */
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -12,34 +15,34 @@ import javax.swing.JOptionPane;
 
 public class HomeValue {
     public static void main(String[] args) throws Exception {
-        int[][] homeValues = csvToArray("AverageHomeValues2021-1997.csv");
-        String introString = "This program will calculate the ideal home \n"+
-            "purchase year between "+homeValues[0][0]+" - "
-            +homeValues[homeValues.length-1][0]+".\nPlease enter your desired price.";
-        int target = 0;
+        int[][] homeValuesArr = csvToArray("AveragehomeValues2021-1997.csv");
+        String introString = "This program will calculate the ideal home \n" +
+                "purchase year between " + homeValuesArr[0][0] + " - "
+                + homeValuesArr[homeValuesArr.length - 1][0] +
+                ".\nPlease enter your desired price.";
+        int target = 0; // placeholder for search target value
         if (target < 1) {
             String userInput = JOptionPane.showInputDialog(introString);
             if (userInput == null) { // Exit cleanly if user hits cancel
                 System.exit(0);
             }
-            try { //validate the input
+            try { // validate the input
                 target = Integer.parseInt(userInput);
-            }
-            catch (NumberFormatException e){
-                JOptionPane.showMessageDialog(null, 
-                    "Invalid Input, please try again.");
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null,
+                        "Invalid Input, please try again.");
             }
         }
-        int bestYear = binarySearchClosest(homeValues, target);
-        JOptionPane.showMessageDialog(null, "The ideal year for you to "+
-                                    "purchase a home was "+bestYear);
+        int closestYear = binarySearchClosest(homeValuesArr, target);
+        JOptionPane.showMessageDialog(null, "The ideal year for you to " +
+                "purchase a home was " + closestYear);
     }
 
     private static int[][] csvToArray(String filename) throws IOException {
-        int[][] outArray = new int[25][2];
+        int[][] outArray = new int[25][2]; // array to hold years and avg price
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
-            String currentLine;
-            int i = 0;
+            String currentLine; // current line read
+            int i = 0; // counter for line number
             while ((currentLine = br.readLine()) != null) {
                 outArray[i][0] = Integer.parseInt(currentLine.split(",")[0]);
                 outArray[i][1] = Integer.parseInt(currentLine.split(",")[1]);
@@ -52,27 +55,30 @@ public class HomeValue {
     }
 
     public static int binarySearchClosest(int[][] array, int target) {
-        if(target < array[0][1]) {
+        if (target < array[0][1]) {
             return array[0][0];
         }
-        if(target > array[array.length-1][1]) {
-            return array[array.length-1][0];
+        if (target > array[array.length - 1][1]) {
+            return array[array.length - 1][0];
         }
         int low = 0;
         int high = array.length - 1;
-        int mid=0;
+        int mid = 0;
         while (high - low != 1) {
             mid = (high + low) / 2;
             if (array[mid][1] == target) {
                 return array[mid][0];
-            }
-            else if (array[mid][1] > target) { //eliminate the right sub-array and continue with the left sub-array
+                // eliminate the right sub-array and continue with the left sub-array
+            } else if (array[mid][1] > target) {
                 high = mid;
-            }
-            else { //mid < target, eliminate the left sub-array and continue with the right sub-array
+            } else { // mid < target, eliminate the left sub-array and continue with the right
+                     // sub-array
                 low = mid;
             }
         }
-        return Math.abs(array[low][1] - target) <= Math.abs(target - array[high][1]) ? array[low][0] : array[high][0];
+        // find the difference between target and high vs target and low, return the appropriate
+        // index year
+        return Math.abs(array[low][1] - target) <= Math.abs(target - array[high][1]) ? array[low][0]
+                : array[high][0];
     }
-}    
+}
