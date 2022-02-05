cb5c1a8674d3cf1b771b86add82d2003e1096c1d
Tom Culpepper <tculpepp@gmail.com>
2022-02-04 21:09:20 -0600

    refactored to split object and main classes. as submitted

--- mod3/case3/src/ClassGrades.java
+++ mod3/case3/src/ClassGrades.java
@@ -11,16 +11,17 @@ import javax.swing.JOptionPane;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.Collections;
 
-public class Student {
-    String firstName;
-    String lastName;
-    int score1;
-    int score2;
-    int score3;
-    int score4;
-    Double avg;
-    char grade;
+class Student implements Comparable<Student> {
+    private String firstName;
+    private String lastName;
+    private int score1;
+    private int score2;
+    private int score3;
+    private int score4;
+    private Double avg;
+    private char grade;
 
     public Student(String lastName, String firstName, int score1,
             int score2, int score3, int score4) {
@@ -44,6 +45,10 @@ public class Student {
         }
     }
 
+    public String getStudentLastName() {
+        return lastName;
+    }
+
     public String toString() {
         return ("Name: " + lastName + ", " + firstName + "\n" +
                 "Scores: " + score1 + "\t" + score2 + "\t" + score3 + "\t" + score4 + "\n" +
@@ -63,7 +68,7 @@ public class Student {
         }
     }
 
-    private static String[] getValidUserInput(String requestMessage, String validationPattern,
+    public static String[] getValidUserInput(String requestMessage, String validationPattern,
             String errorMessage) {
         while (true) {
             String rawUserInput = JOptionPane.showInputDialog(requestMessage);
@@ -77,18 +82,28 @@ public class Student {
         }
     }
 
+    @Override // method to sort based on last name
+    public int compareTo(Student comparestu) {
+        String compareLastName = ((Student) comparestu).getStudentLastName().toLowerCase();
+        return this.lastName.toLowerCase().compareTo(compareLastName);
+    }
+}
+
+
+public class ClassGrades {
     public static void main(String[] args) {
         ArrayList<Student> classGrades = new ArrayList<>(10); // to hold student objects
         boolean addAnother = true;
         JOptionPane.showMessageDialog(null,
-                "This program will collect student names and test scores\nand then report averages and grades for each student.");
+                "This program will collect student names and test scores\n"
+                        + "and then report averages and grades for each student.");
         while (addAnother) {
             // regex matches "name, name"
-            String[] studentName = getValidUserInput(
+            String[] studentName = Student.getValidUserInput(
                     "Student Name (Last, First):", "^[A-Za-z]*\\,(\\s)?[A-Za-z]*$",
                     "Invalid Entry.\nPlease enter 'Last, First'");
             // regex matches csv format for 4 ints between 0-100
-            String[] scores = getValidUserInput(
+            String[] scores = Student.getValidUserInput(
                     "Last 4 Test Scores:\n(01, 02, 03, 04)",
                     "^?\\d{1,3}\\,(\\s)?\\d{1,3}\\,(\\s)?\\d{1,3}\\,(\\s)?\\d{1,3}$",
                     "Invalid entry format.\nPlease enter only whole numbers separated by commas");
@@ -102,9 +117,10 @@ public class Student {
                 addAnother = false;
             }
         }
+        Collections.sort(classGrades);
         StringBuilder classReport = new StringBuilder(); // create the string for output
-        for (Student student : classGrades) {
-            classReport.append(student.toString());
+        for (Student stud : classGrades) {
+            classReport.append(stud.toString());
         }
         JOptionPane.showMessageDialog(null, classReport);
     }
