1e93eee46ed1071fb8fc497f29411cbd528ab4b3
Tom Culpepper <tculpepp@gmail.com>
2022-02-11 21:12:57 -0500

    initial framework case4

---
+++ mod4/case4/src/LinkedListDemo.java
@@ -0,0 +1,15 @@
+/*
+ * 1. Create and populate a LinkedList using a one dimensional array.
+ * 
+ * 2. Add 6 numbers to this LinkedList.
+ * 
+ * 3. Search this LinkedList and find the smallest number.
+ * 
+ * 4. Display the number you get from step 3.
+ */
+
+public class LinkedListDemo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
